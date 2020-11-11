001dd8f156ab5ef603e54abb064783cd9d42fbaa
wangpengcheng <wangpengcheng@lixiang.com>
2020-11-11 15:55:38 +0800

    nestjs 对接 rocketmq

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
@@ -12,6 +13,26 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, options);
   SwaggerModule.setup('api', app, document);
+
+  const httpClient = require('urllib');
+  const Consumer = require('ali-ons').Consumer;
+  const consumer = new Consumer({
+    httpclient: httpClient,
+    // accessKeyId: 'your-accessKeyId',
+    // accessKeySecret: 'your-AccessKeySecret',
+    topic: 'AISP_BUSINESS_MSG',
+    consumerGroup: 'rookie-nestjs',
+    // namespace: '', // aliyun namespace support
+    // isBroadcast: true,
+    nameSrv: '192.168.47.183:9876',
+  });
+
+  consumer.subscribe('AISP_BUSINESS_MSG', '*', async msg => {
+    console.log(`receive message, msgId: ${msg.properties.UNIQ_KEY} offSetMsgId: ${msg.msgId}, body: ${msg.body.toString()} msg:`);
+    return; // you can return ACTION_RETRY, then this message will be directly retried
+  });
+
+  consumer.on('error', err => console.log(err));
 
   await app.listen(3000);
 }
